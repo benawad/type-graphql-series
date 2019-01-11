7f524f8449b0f84a913330c1f7ae789df2f2c33d
Ben Awad <benawad97@gmail.com>
2019-01-10 19:58:36 -0600

    login

--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,55 @@ import { ApolloServer } from "apollo-server-express";
 import Express from "express";
 import { buildSchema, formatArgumentValidationError } from "type-graphql";
 import { createConnection } from "typeorm";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import cors from "cors";
 
 import { RegisterResolver } from "./modules/user/Register";
+import { redis } from "./redis";
+import { LoginResolver } from "./modules/user/Login";
+import { MeResolver } from "./modules/user/Me";
 
 const main = async () => {
   await createConnection();
 
   const schema = await buildSchema({
-    resolvers: [RegisterResolver]
+    resolvers: [MeResolver, RegisterResolver, LoginResolver]
   });
 
   const apolloServer = new ApolloServer({
     schema,
-    formatError: formatArgumentValidationError
+    formatError: formatArgumentValidationError,
+    context: ({ req }: any) => ({ req })
   });
 
   const app = Express();
+
+  const RedisStore = connectRedis(session);
+
+  app.use(
+    cors({
+      credentials: true,
+      origin: "http://localhost:3000"
+    })
+  );
+
+  app.use(
+    session({
+      store: new RedisStore({
+        client: redis as any
+      }),
+      name: "qid",
+      secret: "aslkdfjoiq12312",
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 1000 * 60 * 60 * 24 * 7 * 365 // 7 years
+      }
+    })
+  );
 
   apolloServer.applyMiddleware({ app });
 
---
+++ src/types/MyContext.ts
@@ -0,0 +1,5 @@
+import { Request } from "express";
+
+export interface MyContext {
+  req: Request;
+}
