6b493a602e76af9df63f9fbc7d90ec057ec826d9
Ben Awad <benawad97@gmail.com>
2019-01-15 20:02:53 -0600

    .resolver.ts

--- src/utils/createSchema.ts
+++ src/utils/createSchema.ts
@@ -2,7 +2,7 @@ import { buildSchema } from "type-graphql";
 
 export const createSchema = () =>
   buildSchema({
-    resolvers: [__dirname + "/../modules/*/*.ts"],
+    resolvers: [__dirname + "/../modules/**/*.resolver.ts"],
     authChecker: ({ context: { req } }) => {
       return !!req.session.userId;
     }
