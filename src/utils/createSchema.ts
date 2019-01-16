b91e7bee5dd2e62cfe31b7a755720302a9ba2dea
Ben Awad <benawad97@gmail.com>
2019-01-16 16:47:44 -0600

    import resolvers

--- src/utils/createSchema.ts
+++ src/utils/createSchema.ts
@@ -1,8 +1,23 @@
 import { buildSchema } from "type-graphql";
+import { ChangePasswordResolver } from "../modules/user/ChangePassword";
+import { ConfirmUserResolver } from "../modules/user/ConfirmUser";
+import { ForgotPasswordResolver } from "../modules/user/ForgotPassword";
+import { LoginResolver } from "../modules/user/Login";
+import { LogoutResolver } from "../modules/user/Logout";
+import { MeResolver } from "../modules/user/Me";
+import { RegisterResolver } from "../modules/user/Register";
 
 export const createSchema = () =>
   buildSchema({
-    resolvers: [__dirname + "/../modules/**/*.resolver.ts"],
+    resolvers: [
+      ChangePasswordResolver,
+      ConfirmUserResolver,
+      ForgotPasswordResolver,
+      LoginResolver,
+      LogoutResolver,
+      MeResolver,
+      RegisterResolver
+    ],
     authChecker: ({ context: { req } }) => {
       return !!req.session.userId;
     }
