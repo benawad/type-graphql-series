a59852197aa3d421fe7a5f5e767e6c770a8f9fe9
paean99 <>
2019-01-19 05:31:22 +0000

    adding Dynamic methods with decorators

--- src/modules/user/CreateUser.ts
+++ src/modules/user/CreateUser.ts
@@ -1,59 +1,86 @@
-import {
-  Resolver,
-  Mutation,
-  Arg,
-  ClassType,
-  InputType,
-  Field,
-  Authorized
-} from "type-graphql";
+import { Resolver, Mutation, Arg, ClassType, Query } from "type-graphql";
 import { RegisterInput } from "./register/RegisterInput";
 import { User } from "../../entity/User";
-import { Product } from "../../entity/Product";
 
-function myNewDecorator(value: any) {
-  console.log("myNewDecorator value: ", value);
-  return function(
-    target: any,
-    propertyKey: string,
-    descriptor: PropertyDescriptor
-  ) {
-    console.log("target: ", target);
-    console.log("propertyKey: ", propertyKey);
-    console.log("descriptor: ", descriptor);
-  };
+const methodList: {
+  createM: (data: any) => void;
+  deleteM: (data: any) => void;
+} = {
+  createM: (data: any) => {
+    console.log("create data: ", data);
+  },
+  deleteM: (data: any) => {
+    console.log("delete data: ", data);
+  }
+};
+
+// @ts-ignore
+interface BaseResolver {
+  createM?: typeof methodList.createM;
+  deleteM?: typeof methodList.deleteM;
+  [key: string]: any;
 }
 
+// @ts-ignore
 function createResolver<T extends ClassType, X extends ClassType>(
   suffix: string,
   returnType: T,
   inputType: X,
   entity: any,
-  decorator: any[]
-) {
-  const decoratorFactory = (decos: any[]): Function => {
-    return (target: any, propertyKey: string, descriptor: PropertyDescriptor) =>
-      decos.map((deco: () => Function) =>
-        deco()(target, propertyKey, descriptor)
-      );
-  };
+  methods: any[]
+): any {
+  function classDecorator(): any {
+    return function(
+      target: any,
+      propertyKey: string,
+      descriptor: PropertyDescriptor
+    ) {
+      methods.forEach(method => {
+        // @ts-ignore
+        target.prototype[method] = methodList[method];
+        const targetMethod = target.prototype[method];
+        const targetPrototype = target.prototype;
+        const methodKey = Object.keys(targetPrototype).find(
+          key => targetPrototype[key] === targetMethod
+        );
+        // @ts-ignore
+        Query(() => User, { nullable: true })(
+          targetPrototype,
+          // @ts-ignore
+          methodKey,
+          {
+            value: targetMethod,
+            writable: true,
+            enumerable: false,
+            configurable: true
+          }
+        );
+      });
+      console.log("target: ", target);
+      console.log("propertyKey: ", propertyKey);
+      console.log("descriptor: ", descriptor);
+    };
+  }
 
+  @classDecorator()
   @Resolver()
   class BaseResolver {
-    @decoratorFactory(decorator)
     @Mutation(() => returnType, { name: `create${suffix}` })
-    create(@Arg("data", () => inputType) data: any) {
+    create(
+      @Arg("data", () => inputType)
+      data: any
+    ) {
       return entity.create(data).save();
     }
+
+    /* constructor() {
+      methods.forEach(method => {
+        BaseResolver.prototype[method] = createM;
+      });
+    } */
   }
 
   return BaseResolver;
-}
-
-@InputType()
-class ProductInput {
-  @Field()
-  name: string;
 }
 
 export const CreateUserResolver = createResolver(
@@ -61,12 +88,5 @@ export const CreateUserResolver = createResolver(
   User,
   RegisterInput,
   User,
-  [Authorized, myNewDecorator]
-);
-export const CreateProductResolver = createResolver(
-  "Product",
-  Product,
-  ProductInput,
-  Product,
-  [Authorized, myNewDecorator]
+  ["createM", "deleteM"]
 );
