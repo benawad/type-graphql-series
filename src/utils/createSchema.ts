1c8ce6f3eab74fea6a6d645fc8a85899141a39cc
Manuel Ruck <github@manuelruck.de>
2019-01-20 13:44:24 +0100

    require resolvers dynamic

--- src/utils/createSchema.ts
+++ src/utils/createSchema.ts
@@ -1,23 +1,8 @@
 import { buildSchema } from "type-graphql";
-import { ChangePasswordResolver } from "../modules/user/ChangePassword";
-import { ConfirmUserResolver } from "../modules/user/ConfirmUser";
-import { ForgotPasswordResolver } from "../modules/user/ForgotPassword";
-import { LoginResolver } from "../modules/user/Login";
-import { LogoutResolver } from "../modules/user/Logout";
-import { MeResolver } from "../modules/user/Me";
-import { RegisterResolver } from "../modules/user/Register";
 
 export const createSchema = () =>
   buildSchema({
-    resolvers: [
-      ChangePasswordResolver,
-      ConfirmUserResolver,
-      ForgotPasswordResolver,
-      LoginResolver,
-      LogoutResolver,
-      MeResolver,
-      RegisterResolver
-    ],
+    resolvers: [`${__dirname}/../modules/**/!(*.test).ts`],
     authChecker: ({ context: { req } }) => {
       return !!req.session.userId;
     }
