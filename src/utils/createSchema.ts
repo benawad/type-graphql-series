46506f5743c1e4b5c431cd1d2d5165f2ce22a461
ManAnRuck <gitHub@manuelruck.de>
2019-02-11 09:56:10 +0100

    get js for builded versions

--- src/utils/createSchema.ts
+++ src/utils/createSchema.ts
@@ -2,7 +2,7 @@ import { buildSchema } from "type-graphql";
 
 export const createSchema = () =>
   buildSchema({
-    resolvers: [`${__dirname}/../modules/**/!(*.test).ts`],
+    resolvers: [`${__dirname}/../modules/**/!(*.test).?(ts|js)`],
     authChecker: ({ context: { req } }) => {
       return !!req.session.userId;
     }
